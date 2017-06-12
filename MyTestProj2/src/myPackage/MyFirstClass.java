a1251194cf8e0dc80778445935f1ff2b541906f9
anandNandhu <anand.nandhu@gmail.com>
2017-06-12 16:46:54 +0530

    test 432

--- MyTestProj2/src/myPackage/MyFirstClass.java
+++ MyTestProj2/src/myPackage/MyFirstClass.java
@@ -5,7 +5,7 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hi ganesh");
+		System.out.println("Hi ganesh2");
 	}
 
 }
